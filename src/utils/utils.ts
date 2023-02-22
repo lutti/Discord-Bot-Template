af2f91a8ffd6266221c2f385a041a6aea488004c
Lutti <lutti.c.amaral@gmail.com>
2023-02-22 15:52:37 -0300

    Function Utility

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,11 @@
-function AcharNumeroNoArray(array: string[]) : number {
-    console.log(array);
-    return 1;
+function AcharNumeroNoArray(arr: string[]) : number | null {
+    for (let i = 0; i < arr.length; i++) {
+        const num = parseInt(arr[i]);
+        if (!isNaN(num)) {
+            return num;
+        }
+    }
+    return null;
 }
 
 export default AcharNumeroNoArray;
